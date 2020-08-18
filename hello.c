58edbe2de9ffbf402ccc4e61590088293a1b6fdb
nbrana2 <nbrana2@huskers.unl.edu>
2020-08-18 14:46:52 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Nataliya Brana
+ * Date: 2020
+ *
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("hello World!\n");
+
+    return 0;
+}
